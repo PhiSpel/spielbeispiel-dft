6d57683648c4d1c36b7bf84155d3e0e66f61c544
PhiSpel <62760840+PhiSpel@users.noreply.github.com>
2023-02-06 17:09:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -148,7 +148,7 @@ with st.expander('Filtered plot'):
     at_filtered = np.fft.ifft(fourierTransform_filtered, n=len(tspan))
     fourierTransform_filtered_plot = fourierTransform_filtered[range(int(np.ceil(len(at) / 2)))]  # Exclude sampling frequency
     handles["timescale"].set_ydata(at_filtered.imag)
-    handles["timescale"][1] = ax1.plot(tspan, at_filtered.real, color='b', linewidth=0.4, label='real')[0]
+    handles["timescale"].append(ax1.plot(tspan, at_filtered.real, color='b', linewidth=0.4, label='real')[0])
     handles["frequencyscale"][0].set_ydata(abs(fourierTransform_filtered_plot.real))
     handles["frequencyscale"][1].set_ydata(abs(fourierTransform_filtered_plot.imag))
     fig.canvas.draw()
