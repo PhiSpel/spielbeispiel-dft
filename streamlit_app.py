11e83d8fac044f86317f4fdb2a989d11fe7fc485
PhiSpel <PhiSpel@users.noreply.github.com>
2023-02-06 16:57:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -147,7 +147,7 @@ with st.expander('Filtered plot'):
     st.write('Filtered tune capping off all frequencies with an amplitude below an amplitude of ' + str(cap))
     at_filtered = np.fft.ifft(fourierTransform_filtered, n=len(tspan))
     fourierTransform_filtered_plot = fourierTransform_filtered[range(int(np.ceil(len(at) / 2)))]  # Exclude sampling frequency
-    handles["timescale"].set_ydata(np.absolute(at_filtered))
+    handles["timescale"].set_ydata(at_filtered.imag)
     handles["frequencyscale"][0].set_ydata(abs(fourierTransform_filtered_plot.real))
     handles["frequencyscale"][1].set_ydata(abs(fourierTransform_filtered_plot.imag))
     fig.canvas.draw()
